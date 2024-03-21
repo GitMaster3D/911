5c18046aebfdfd8762db49435f486f58af4e9a97
NorisskNoFun <lukas6306@web.de>
2024-03-21 09:25:03 +0100

    ich bin ein spast

--- Assets/Scripts/ScoreCounter.cs
+++ Assets/Scripts/ScoreCounter.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ScoreCounter : MonoBehaviour
 {
+    public static ScoreCounter insance;
+    public int score;
     // Start is called before the first frame update
     void Start()
     {
+        insance =this;
         
     }
 
