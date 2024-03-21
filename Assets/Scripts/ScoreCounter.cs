55def4aee70f03825ee84292f3854f06a6632653
NorisskNoFun <lukas6306@web.de>
2024-03-21 10:44:12 +0100

    Ich mache alles kapput

--- Assets/Scripts/ScoreCounter.cs
+++ Assets/Scripts/ScoreCounter.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreCounter : MonoBehaviour
 {
+    public TextMeshProUGUI hurensohn;
     public static ScoreCounter insance;
-    public int score;
+    int Penis;
+    public int score{get{
+        return Penis;
+    } set{
+        Penis = value;
+        hurensohn.text = Penis.ToString();
+    }}
     // Start is called before the first frame update
     void Start()
     {
